de6f744dd8d2e4eb69aef553dc67b42371fa1871
Dawid <you@example.com>
2020-05-02 11:22:28 +0200

    added google sheets to form

--- script.js
+++ script.js
@@ -25,3 +25,17 @@ navSlide();
 
 
 
+var $form = $('form#test-form'),
+    url = 'https://script.google.com/macros/s/AKfycbzJ89fWrBhbmKN5ABGw1swt598bVqe7fnBFdlciq-yr6GEyGcs/exec'
+
+$('#submit-form').on('click', function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serializeObject()
+  }).success(
+    // do something
+  );
+})
